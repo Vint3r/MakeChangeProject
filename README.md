6a070c2335fe762dfc23ffe8ec0f030ca83ab07e
David Norris <Norris.David.Dev@gmail.com>
2019-09-14 12:39:02 -0600

    Updating the README and the finished project

--- README.md
+++ README.md
@@ -2,11 +2,30 @@
 
 ### First week homework for Skill Distillery
 
+The first homework project I had to do for my class in Skill Distillery, its a little clunky in how it does
+what it does but it works.
+
 ### Overview
 
-This program simulates a cash transaction interface. (kinda)
+This program simulates a cash transaction interface.
+
 ### How to run
+
+This program will greet you and create a simple menu displaying the users options,
+asking if the user wants to quit, add a item to their 'cart' or pay for everything they currently have.
+All that you need to do is type the number for the option you wish (or the word) and follow the prompts
+there after. After you are finished putting items in your cart you can pay or quit, if you haven't paid
+the program will cancel the transaction and you get nothing, if you pay over the amount it will calculate what
+coins and bills you receive as change, and if you pay the exact amount it will tell you so.
 
 ### Technologies/topics used
 
+I used a method to do all the math for finding your change due back, with a few while loops and if else if with
+a single switch statement to do all the logic for this program.
+
 ### Lessons
+
+Learned and practiced with creating and converting different variables along with dealing with the limitations of
+the different types of variables such as doubles. I also tried to add a bit of error handling, albeit maybe
+not as much as it should need but some to try and keep the program from crashing if the user doesn't input what
+it wants.
--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -1,9 +1,112 @@
+import java.util.Scanner;
 
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		double amountOwed = 0.0, amountPaid = 0.0, amountMore = 0;
+		int convertOwed = 0, convertPaid = 0;
+		boolean choice = true;
 
+		System.out.println("Welcome to the cash register!");
+
+		while (choice) {
+			System.out.println("Please choose an option:");
+			System.out.println("1.) Add item to cart");
+			System.out.println("2.) Pay for your current items");
+			System.out.println("3.) Quit or cancel (will terminate all transactions)");
+			String userIn = input.next();
+
+			switch (userIn.toLowerCase()) {
+			case "add item":
+			case "add":
+			case "1":
+			case "one":
+				System.out.println("How much is the item, your current balance due is: $" + amountOwed);
+				amountOwed += input.nextDouble();
+				System.out.println("Thank you, your new balance due is: $" + amountOwed);
+				continue;
+			case "pay":
+			case "2":
+				amountMore = amountOwed - amountPaid;
+				System.out.println("How much will you tender? Your current balance due is $" + amountMore);
+				amountPaid = input.nextDouble();
+				convertOwed = (int) (amountOwed * 100);
+				convertPaid += (int) (amountPaid * 100);
+
+				if (convertOwed > convertPaid) {
+					amountMore = amountOwed - amountPaid;
+					System.out.printf("You paid $%.2f you need to pay $%.2f more to buy everything.\n", amountPaid, amountMore);
+				} else if (convertOwed == convertPaid) {
+					System.out.println("Thank you, you paid the exact amount!");
+					amountOwed = 0;
+					amountPaid = 0;
+					convertOwed = 0; 
+					convertPaid = 0;
+				} else {
+					String changeBack = getChange(convertOwed, convertPaid);
+					System.out.println("Your change back is: " + changeBack);
+					amountOwed = 0;
+					amountPaid = 0;
+					convertOwed = 0; 
+					convertPaid = 0;
+				}
+				continue;
+			case "3":
+			case "quit":
+			case "cancel":
+				if (amountOwed != 0) {
+					System.out.println("Thank you, your transaction has been cancelled. Have a great day!");
+					amountOwed = 0;
+					choice = false;
+					break;
+				} else {
+					System.out.println("Thank you for shopping with us today!");
+					choice = false;
+					break;
+				}
+			default:
+				System.out.println("That is not a valid option, please try again.");
+				continue;
+			}
+		}
+
+		input.close();
+
+	}
+
+	public static String getChange(int amountOwed, int amountPaid) {
+		int totalChange = amountPaid - amountOwed;
+		String changeBack = "";
+
+		while (totalChange != 0) {
+			if (totalChange >= 2000) {
+				totalChange -= 2000;
+				changeBack += "A 20 dollar bill,";
+			} else if (totalChange >= 1000) {
+				totalChange -= 1000;
+				changeBack += " a 10 dollar bill,";
+			} else if (totalChange >= 500) {
+				totalChange -= 500;
+				changeBack += " a 5 dollar bill,";
+			} else if (totalChange >= 100) {
+				totalChange -= 100;
+				changeBack += " a 1 dollar bill,";
+			} else if (totalChange >= 25) {
+				totalChange -= 25;
+				changeBack += " a quarter,";
+			} else if (totalChange >= 10) {
+				totalChange -= 10;
+				changeBack += " a dime,";
+			} else if (totalChange >= 5) {
+				totalChange -= 5;
+				changeBack += " a nickel,";
+			} else {
+				totalChange -= 1;
+				changeBack += " a penny,";
+			}
+		}
+		return changeBack;
 	}
 
 }
